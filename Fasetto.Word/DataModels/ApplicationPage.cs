007639b47f3cf3661603a8c38babe1c74e13a8c3
Usama-Aziz <=>
2021-06-16 20:02:23 +0500

    Add project files.

---
+++ Fasetto.Word/DataModels/ApplicationPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreyBase.DataModels
+{
+	/// <summary>
+	/// A page of application
+	/// </summary>
+	public enum ApplicationPage
+	{
+		Login,
+		Dashboard
+	}
+}
---
+++ Fasetto.Word/ValueConvertors/ApplicationPageValueConvertor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GreyBase.DataModels;
+using GreyBase.Pages;
+
+namespace GreyBase.ValueConvertors
+{
+	/// <summary>
+	/// Converts the <see cref="ApplicationPage"/> to an actual view/page
+	/// </summary>
+	public class ApplicationPageValueConvertor : BaseValueConvertor<ApplicationPageValueConvertor>
+	{
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			switch ((ApplicationPage)value)
+			{
+				default:
+					Debugger.Break();
+					return null;
+				case ApplicationPage.Dashboard:
+					return new DashboardPage();
+			}
+		}
+
+		public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
