b82c467002123f7b2aa51a15dd5a2ded833f0e34
Usama-Aziz <=>
2021-06-17 08:03:26 +0500

    Added ViewModels for SideMenu buttons.

---
+++ Fasetto.Word/ViewModel/SideMenuItemDesignModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreyBase
+{
+	public class SideMenuItemDesignModel : SideMenuItemViewModel
+	{
+
+		public static SideMenuItemDesignModel Instance => new SideMenuItemDesignModel();
+
+		public SideMenuItemDesignModel()
+		{
+			Name = "Dashboard";
+			IconPath = "pack://application:,,,/GreyBase;component/Images/Icons/icons8_home_60px.png";
+		}
+	}
+}
